4226bc1bb5d897eaf390d8e619eeb94f3ec42e1e
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-08 12:59:50 +0530

    Create Coverting_kg_to_miles.py
    6th

---
+++ Coverting_kg_to_miles.py
@@ -0,0 +1,6 @@
+# converint kilometers to miles
+kilometer =  float(input("Enter the no. in decimal: "))
+conversion = 0.621
+result = kilometer * conversion
+print(f"{kilometer} kilometer is equal to {result} miles")
+print(result)
