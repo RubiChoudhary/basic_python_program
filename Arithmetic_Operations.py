e25fbd76fdda367bb1f02ee9a8cecbb1014b0f44
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-08 11:45:15 +0530

    Create Arithmetic_Operations.py
    second

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,13 @@
+# Addition
+addition_num = int(input())
+addition_num1 = int(input())
+sum_result = addition_num1 + addition_num
+print("Addition of {}, {} is {}".format(addition_num, addition_num1, sum_result))
+print(sum_result)
+
+# Substraction
+num = float(input())
+num2 = float(input())
+Substraction_result = num - num2
+substraction_result1 = "{:.2f}".format(Substraction_result)
+print("Sustracion of {}, {} is {}".format(num, num2, substraction_result1))
