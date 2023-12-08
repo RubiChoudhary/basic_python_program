c6b3dcb14c3eee860a002c29e1471ca13aaf5c9d
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-08 17:58:15 +0530

    Create about_number.py
    11th

---
+++ about_number.py
@@ -0,0 +1,9 @@
+Write a Python Program to Check if a Number is Positive, Negative or Zero.
+num=int(input())
+if num>0:
+    print("Number is postive", num)
+elif num<0:
+    print("Number is negative", num)
+else:
+    print("number is zero")
+    
