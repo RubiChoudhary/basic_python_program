5bc87977eb3b5e19573030a52c2f5362c27b9b7b
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-09 14:18:15 +0530

    Create conv_num_to_bin-oct-hex.py
    24th

---
+++ conv_num_to_bin-oct-hex.py
@@ -0,0 +1,8 @@
+# Write a Python Program to Convert Decimal to Binary, Octal and Hexadecimal
+num = int(input("enter number: "))
+Binary = bin(num)
+print("Number in Binary: ",Binary)
+Octal = oct(num)
+print("Number in Octal: ",Octal)
+Hexadecimal = hex(num)
+print("Number in Hexadecimal: ",Hexadecimal)
