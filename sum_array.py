cf94fb432593c86a0a311fc8bd5a128fe65ed1b6
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-10 12:07:23 +0530

    Update sum_array.py
    32nd

--- sum_array.py
+++ sum_array.py
@@ -1,8 +1,8 @@
 # Write a Python Program to find sum of array.
-my_list = [1,2,3,4,5]
+my_list = input().split()
 add = 0
 for i in my_list:
-    add = add+i
+    add = add+ int(i)
     print("sum of number in my_list: ",add)
 # output:- 
 # sum of number in my_list: 15
