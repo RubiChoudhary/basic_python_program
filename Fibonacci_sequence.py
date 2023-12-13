7385f7560063daee0501acd21b722b979d835ad4
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-12-13 15:36:51 +0530

    Update Fibonacci_sequence.py

--- Fibonacci_sequence.py
+++ Fibonacci_sequence.py
@@ -1,20 +1,18 @@
 # Write a Python Program to Print the Fibonacci sequence.
-num = int(input())
-start1, start2 = 0, 1
-count = 0
-while count < num:
-    print(start1)
-    temp = start1 + start2
-    start1 = start2
-    start2 = temp
-    count  = count+1
+n=int(input())
+if n ==0:
+    print(1)
+elif n==1:
+    print(1)
+else:
+    print(1,1, end= ' ')
+    a= 1
+    b= 1
+    for i in range(2, n+1):
+        c= a+b
+        print(c, end=' ')
+        a=b
+        b = c
 
 output:-
-0
-1
-1
-2
-3
-5
-8
-13
+0 1 1 2 3 5 8 13
