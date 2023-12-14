b3c387a4673da353abbe0c04ed12be6fa27ffb03
Rubi Choudhary <rubichaudhary2000@gmail.com>
2023-12-14 11:44:50 +0530

    matrix_row_column

---
+++ matrix_row_column.py
@@ -0,0 +1,57 @@
+row  = 3
+column= 5
+matrix = []
+sum = 0
+for i in range(row):
+    temp = []
+    for j in range(column):
+        sum = i+j
+        temp.append(sum)
+    matrix.append(temp)
+print(matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
